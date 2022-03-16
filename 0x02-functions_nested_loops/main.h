3e82e3304093e1c378bc44c340ab2f5b7e180428
Hennydev <adewunmienitan1755@gmail.com>
2022-03-16 01:40:59 +0100

    work

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,23 @@
-#include "main.h"
-/**
-* main - Entry point
-* write a program tha prints Holberton, followed by a new line.
-* Return: Always (Success)
-*/
-int main(void)
-{
-	char *ch = "_putchar";
+int _putchar(char);
 
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
-	_putchar('\n');
-return (0);
-}
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
